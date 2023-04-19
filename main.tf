422edb6202c8b085bece0c303fe619fe2310181e
Ryan Fallon <ryano0oceros@github.com>
2023-04-19 14:00:05 -0400

    add aws creds

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = "ex-${basename(path.cwd)}"
-  region = "eu-west-1"
+  region = "us-east-1"
 
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = "10.24.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     Example    = local.name
-    GithubRepo = "terraform-aws-vpc"
-    GithubOrg  = "terraform-aws-modules"
+    GithubRepo = "epa-at-318-tf-aws-vpc"
+    GithubOrg  = "ryano0oceros"
   }
 }
 
@@ -23,7 +23,7 @@ locals {
 ################################################################################
 
 module "vpc" {
-  source = "../../"
+  source = "ryano0oceros/epa-at-318-tf-aws-vpc"
 
   name = local.name
   cidr = local.vpc_cidr
